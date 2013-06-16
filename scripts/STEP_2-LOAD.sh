27419ce002e6f24a2ac67c056ce9c77e8b42c896
Vincent Picavet <vincent.picavet@oslandia.com>
2013-06-16 15:29:58 +0200

    Add conditional drops.

--- scripts/STEP_2-LOAD.sh
+++ scripts/STEP_2-LOAD.sh
@@ -1,4 +1,7 @@
 # Create all Schemas
+psql postgis -c "DROP SCHEMA if exists admin;"
+psql postgis -c "DROP SCHEMA if exists hydro;"
+psql postgis -c "DROP SCHEMA if exists rff;"
 psql postgis -c "CREATE SCHEMA admin"
 psql postgis -c "CREATE SCHEMA hydro"
 psql postgis -c "CREATE SCHEMA rff"
@@ -9,7 +12,7 @@ shp2pgsql -s 2154 -d -I -D -W LATIN1 ../../data/carthage/COURS_D_EAU.SHP hydro.c
 
 
 # CSV data load
-psql postgis -c "DROP TABLE rff.station";
+psql postgis -c "DROP TABLE IF EXISTS rff.station";
 psql postgis -c "CREATE TABLE rff.station (name varchar, type varchar, region varchar, lat double precision, lon double precision);"
 psql postgis -c "COPY rff.station FROM '/home/vagrant/workshop/data/rff/gare_2012.csv' DELIMITER ',' CSV;"
 
